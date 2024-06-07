e85987b940ec30e55ba697cf83dc628d9f90a569
tohilak <tohilak121@gmail.com>
2024-06-07 08:42:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,29 +8,16 @@ async function generateLink() {
     return;
   }
 
-  // Generate the TinyURL
+  // Generate TinyURL
   var tinyUrl = await getTinyUrl(linkInput);
 
-  // Display the TinyURL as a button
+  // Display the link as a button
   linkOutput.innerHTML = '<a href="' + tinyUrl + '" class="button">Click here to get the download link</a>';
 }
 
-async function getTinyUrl(url) {
-  var response = await fetch('https://tinyurl.com/api-create.php?url=' + encodeURIComponent(url));
-  var data = await response.text();
-  return data;
+async function getTinyUrl(originalUrl) {
+  var apiUrl = 'https://tinyurl.com/api-create.php?url=' + encodeURIComponent(originalUrl);
+  var response = await fetch(apiUrl);
+  var tinyUrl = await response.text();
+  return tinyUrl;
 }
-
-function redirectToOriginalLink() {
-  var urlParams = new URLSearchParams(window.location.search);
-  var originalLink = urlParams.get('link');
-  
-  // Redirect to the original link
-  window.location.href = originalLink;
-}
-
-// Prevent going back to the main page
-history.pushState(null, null, window.location.href);
-window.addEventListener('popstate', function () {
-  history.pushState(null, null, window.location.href);
-});
